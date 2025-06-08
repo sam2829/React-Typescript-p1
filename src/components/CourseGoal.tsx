7d69fd40cfe1cd52520b904f91cf5327a0b6912e
sam2829 <samscarisbrick@outlook.com>
2025-06-08 11:31:19 +0100

    initial commit with adding Header and corse goal component

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,35 @@
+import type { PropsWithChildren } from "react";
+
+// interface CourseGoalProps = {
+//   title: string;
+//   children: ReactNode;
+// };
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
+
+// optional version as arrow function but must import FC
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import type { PropsWithChildren } from "react";
+
+type HeaderProps = PropsWithChildren<{
+  image: {
+    src: string;
+    alt: string;
+  };
+}>;
+
+export default function Header({ image, children }: HeaderProps) {
+  return (
+    <header>
+      <img {...image} />
+      {children}
+    </header>
+  );
+}
